1a7c953a4e18e3ab8b3f40528b581899b7814dd5
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-23 19:03:15 +0000

    migration of char counter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,23 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Auto-resize to fit content
+//AUTO-RESIZE TO FIT CONTENT - (auto-resizer.js)
 $(document).ready(function () {
   $("textarea").autoResize();
+});
+
+// CHARACTER COUNTER
+$(document).ready(function () {
+  $("#tweet-text")
+    .keyup(function () {
+      const counter = 140;
+      let charCount = $(this).val().length;
+
+      counter - charCount < 0
+        ? $(".counter").css("color", "#AD0F00")
+        : $(".counter").css("color", "#343A35");
+
+      $(".counter").text(counter - charCount);
+    })
+    .keyup();
 });
