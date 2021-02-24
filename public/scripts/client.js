0eed1fdab939e3d5fc4ae197340e141510779805
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-24 14:01:29 +0000

    create function tweet element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -24,3 +24,33 @@ $(document).ready(function () {
     })
     .keyup();
 });
+
+const createTweetElement = (obj) => {
+  // Takes in a tweet object
+return `
+  <br>
+  <article class="recent-tweets">
+  <header class="tweetpost">
+    <div class="tweeter-profile">
+      <img src=${obj.user.avatars} alt="Newton" />
+      <h3>${obj.user.name}</h3>
+    </div>
+    <h3 class="profile-link">${obj.user.handle}</h3>
+  </header>
+  <div class="content">
+    <p>
+      ${obj.content.text}
+    </p>
+  </div>
+  <footer>
+    <p class="tweet-date">${obj.created_at}</p>
+    <div class="tweet-reactions">
+      <i class="fab fa-font-awesome-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </footer>
+</article>
+<br>`
+  // returns a tweet <article> element
+};
