d89d85931fab0a63232318b2962a3a3a7276d875
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-24 17:35:40 +0000

    add comments and time since functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,12 +4,17 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-//AUTO-RESIZE TO FIT CONTENT - (auto-resizer.js)
+/**
+ * Resizes the container for a new tweet
+ * see auto-resizer.js
+ */
 $(document).ready(function () {
   $("textarea").autoResize();
 });
 
-// CHARACTER COUNTER
+/**
+ * Character count for a new tweet
+ */
 $(document).ready(function () {
   $("#tweet-text")
     .keyup(function () {
@@ -25,7 +30,41 @@ $(document).ready(function () {
     .keyup();
 });
 
-// ****************DON'T FORGET THE DATE!!!!!! RIGHT NOW ITS IN MS FORMAT!!!!!
+/**
+ * Gives the elapsed time since 
+ * @author fearofawhackplanet
+ * @link https://stackoverflow.com/users/207752/fearofawhackplanet
+ */
+const timeDifference = (previous) => {
+  let msPerMinute = 60 * 1000;
+  let msPerHour = msPerMinute * 60;
+  let msPerDay = msPerHour * 24;
+  let msPerMonth = msPerDay * 30;
+  let msPerYear = msPerDay * 365;
+
+  let current = Date.now();
+  let elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    return Math.round(elapsed / 1000) + " seconds ago";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + " hours ago";
+  } else if (elapsed < msPerMonth) {
+    return Math.round(elapsed / msPerDay) + " days ago";
+  } else if (elapsed < msPerYear) {
+    return Math.round(elapsed / msPerMonth) + " months ago";
+  } else {
+    return Math.round(elapsed / msPerYear) + " years ago";
+  }
+};
+
+/**
+ * From a tweet objects returns an article
+ * @param {object} tweetObj
+ * @returns {HTMLBodyElement} New Article (tweet)
+ */
 const createTweetElement = (tweetObj) => {
   // Takes in a tweet tweetObject
   return `
@@ -33,7 +72,7 @@ const createTweetElement = (tweetObj) => {
   <article class="recent-tweets">
   <header class="tweetpost">
     <div class="tweeter-profile">
-      <img src=${tweetObj.user.avatars} alt="Newton" />
+      <img src="${tweetObj.user.avatars}" alt="Newton" />
       <h3>${tweetObj.user.name}</h3>
     </div>
     <h3 class="profile-link">${tweetObj.user.handle}</h3>
@@ -44,7 +83,7 @@ const createTweetElement = (tweetObj) => {
     </p>
   </div>
   <footer>
-    <p class="tweet-date">${tweetObj.created_at}</p>
+    <p class="tweet-date">${timeDifference(tweetObj.created_at)}</p>
     <div class="tweet-reactions">
       <i class="fab fa-font-awesome-flag"></i>
       <i class="fas fa-retweet"></i>
@@ -56,7 +95,11 @@ const createTweetElement = (tweetObj) => {
   // returns a tweet <article> element
 };
 
-// LOOP THROUGH THE ARRAY OF TWEET OBJECTS
+/**
+ * by using createTweetElement function appends the result to the page
+ * @param {array} tweetsArr 
+ * @returns {object}
+ */
 const renderTweets = (tweetsArr) => {
   $.each(tweetsArr, (i, tweetObj) => {
     // creates an article element out of each object and appends
@@ -66,29 +109,30 @@ const renderTweets = (tweetsArr) => {
 };
 
 // CALL THE FUNCTION TO LOAD DB
-$(document).ready(function() {
+$(document).ready(function () {
   renderTweets([
     {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
       },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
+      content: {
+        text:
+          "If I have seen further it is by standing on the shoulders of giants",
       },
-      "created_at": 1613821770770
+      created_at: 1613821770770,
     },
     {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
       },
-      "content": {
-        "text": "Je pense , donc je suis"
+      content: {
+        text: "Je pense , donc je suis",
       },
-      "created_at": 1613908170770
-    }
+      created_at: 1613908170770,
+    },
   ]);
-})
+});
