89d4f9a72649a7b40616438625c169649583d851
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-24 21:41:38 +0000

    restructuring & ajax reload page on tweet post

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,35 +5,6 @@
  */
 
 /**
- * Resizes the container for tweets
- * see auto-resizer.js
- */
-$(document).ready(function () {
-  $("textarea").autoResize();
-});
-$(document).ready(function () {
-  $(".recent-tweets").autoResize();
-});
-
-/**
- * Character count for a new tweet
- */
-$(document).ready(function () {
-  $("#tweet-text")
-    .keyup(function () {
-      const counter = 140;
-      let charCount = $(this).val().length;
-
-      counter - charCount < 0
-        ? $(".counter").css("color", "#AD0F00")
-        : $(".counter").css("color", "#343A35");
-
-      $(".counter").text(counter - charCount);
-    })
-    .keyup();
-});
-
-/**
  * Gives the elapsed time since
  * @author fearofawhackplanet
  * @link https://stackoverflow.com/users/207752/fearofawhackplanet
@@ -62,6 +33,14 @@ const timeDifference = (previous) => {
     return Math.round(elapsed / msPerYear) + " years ago";
   }
 };
+/**
+ * For them malicious birds! :D
+ */
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 /**
  * From a tweet objects returns an article
@@ -82,7 +61,7 @@ const createTweetElement = (tweetObj) => {
   </header>
   <div class="content">
     <p>
-      ${tweetObj.content.text}
+      ${escape(tweetObj.content.text)}
     </p>
   </div>
   <footer>
@@ -98,50 +77,83 @@ const createTweetElement = (tweetObj) => {
   // returns a tweet <article> element
 };
 
-/**
- * by using createTweetElement function appends the result to the page
- * @param {array} tweetsArr
- * @returns {object}
- */
-const renderTweets = (tweetsArr) => {
-  let timeArray = tweetsArr.sort((a, b) => a.created_at - b.created_at).reverse();
-  $.each(timeArray, (i, tweetObj) => {
-    // creates an article element out of each object and appends
-    // it to the page section
-    $("#tweets-container").append(createTweetElement(tweetObj));
-  });
-};
-
-/**
- * Fetching tweets with Ajax
- * On a successful fetch renderTweets the returning data
- */
 $(document).ready(function () {
-  const url = "http://localhost:8080/tweets"
-  $.ajax({
-    url,
-    method: 'GET',
-  })
-    .done((data) => {
-      // success! we're getting the data back :)
-      console.log(data);
-      renderTweets(data);
+  /**
+   * by using createTweetElement function appends the result to the page
+   * @param {array} tweetsArr
+   * @returns {object}
+   */
+  const renderTweets = (tweetsArr) => {
+    let timeArray = tweetsArr
+      .sort((a, b) => a.created_at - b.created_at)
+      .reverse();
+    $.each(timeArray, (i, tweetObj) => {
+      // creates an article element out of each object and appends
+      // it to the page section
+      $("#tweets-container").append(createTweetElement(tweetObj));
+    });
+  };
 
+  /**
+   * Fetching tweets with Ajax
+   * On a successful fetch renderTweets the returning data
+   */
+  const loadTweets = () => {
+    const url = "http://localhost:8080/tweets";
+    $.ajax({
+      url,
+      method: "GET",
     })
-    .fail((err) => {
-      // fail case
-      console.log(err.message);
-    })
-});
+      .done((data) => {
+        $("#tweets-container").empty();
+        // success! we're getting the data back :)
+        renderTweets(data);
+      })
+      .fail((err) => {
+        // fail case
+        console.log(err.message);
+      });
+  };
 
-/**
- * Form Submission via JQuery
- */
-$(document).ready(function () {
+  $("#tweets-container").on("reload", loadTweets).trigger("reload");
+
+  /**
+   * Resizes the container for new tweets for long messages
+   * see auto-resizer.js
+   */
+  $("textarea").autoResize();
+
+  /**
+   * Character count for a new tweet
+   */
+  $("#tweet-text")
+    .keyup(function () {
+      const counter = 140;
+      let charCount = $(this).val().length;
+
+      counter - charCount < 0
+        ? $(".counter").css("color", "#AD0F00")
+        : $(".counter").css("color", "#343A35");
+
+      $(".counter").text(counter - charCount);
+    })
+    .keyup();
+
+  /**
+   * Form Submission via JQuery
+   */
   $(".tweet-form").submit(function (event) {
     event.preventDefault();
-    let message = $(this).serialize();
-    $.post("/tweets", message);
-    $("#tweet-text").val('');
+    if ($("#tweet-text").val().length === 0) {
+      alert("We couldn't hear the humming, please type!!");
+    } else if ($("#tweet-text").val().length > 140) {
+      alert("You have a lot to hum, please hum shorter!!");
+    } else {
+      let message = $(this).serialize();
+      $.post("/tweets", message).then(() => {
+        $("#tweets-container").trigger("reload");
+        $("#tweet-text").val("");
+      });
+    }
   });
 });
